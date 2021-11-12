d350660e3e943843cd7a474bf44b74d33662ddd0
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2021-11-12 12:53:52 +0100

    Add files via upload

---
+++ CompagniaSharedMobility/src/Database.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    private List <Utente> utenti = new ArrayList<Utente>();
+    private List <Automobile> automobili = new ArrayList<Automobile>();
+    private List <Scooter> scooters = new ArrayList<Scooter>();
+    private List <Furgoncino> furgoncini = new ArrayList<Furgoncino>();
+    private List <MonopattinoElettrico> monopattiniElettrici = new ArrayList<MonopattinoElettrico>();
+    private List <Bici> biciclette = new ArrayList<Bici>();
+    private List <Noleggio> noleggi = new ArrayList<Noleggio>();
+}
