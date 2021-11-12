abbfc1cd99ef6695a90799d53d28c6a6dc48a888
Niki <80227107+kan3ki1997@users.noreply.github.com>
2021-11-12 12:49:06 +0100

    variabili e metodi creati

---
+++ CompagniaSharedMobility/src/Noleggio.java
@@ -0,0 +1,65 @@
+import java.util.Date;
+
+public class Noleggio {
+
+    private Utente utente;
+    private Veicolo veicolo;
+    private Date dataInizio;
+    private Date dataFine;
+
+
+    // metodi
+    public void registraUtente(){
+
+    }
+
+    public void loginUtente(){
+
+    }
+
+    public void veicoliDisponibili(){
+
+    }
+
+    public void affittoVeicolo(){
+
+    }
+
+    public void fineNoleggio(){
+
+    }
+
+    // getters and setters
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    public Veicolo getVeicolo() {
+        return veicolo;
+    }
+
+    public void setVeicolo(Veicolo veicolo) {
+        this.veicolo = veicolo;
+    }
+
+    public Date getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(Date dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public Date getDataFine() {
+        return dataFine;
+    }
+
+    public void setDataFine(Date dataFine) {
+        this.dataFine = dataFine;
+    }
+}
