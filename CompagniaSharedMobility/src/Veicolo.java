c840a2d8a7b5a922d8d0d80d7878fa6cf14f9851
Alessandro <alessandro.ghio@protonmail.com>
2021-11-12 15:04:11 +0200

    Auto stash before merge of "main" and "origin/main"

--- CompagniaSharedMobility/src/Veicolo.java
+++ CompagniaSharedMobility/src/Veicolo.java
@@ -3,4 +3,46 @@ public abstract class Veicolo {
    double[] posizioneGPS;
    boolean affittato;
    double tariffa;
+
+   public Veicolo(int id, double[] posizioneGPS, boolean affittato, double tariffa) {
+      this.id = id;
+      this.posizioneGPS = posizioneGPS;
+      this.affittato = affittato;
+      this.tariffa = tariffa;
+   }
+
+   // getters
+   public int getId() {
+      return id;
+   }
+
+   public double[] getPosizioneGPS() {
+      return posizioneGPS;
+   }
+
+   public boolean isAffittato() {
+      return affittato;
+   }
+
+   public double getTariffa() {
+      return tariffa;
+   }
+
+   // setters
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public void setPosizioneGPS(double[] posizioneGPS) {
+      this.posizioneGPS = posizioneGPS;
+   }
+
+   public void setAffittato(boolean affittato) {
+      this.affittato = affittato;
+   }
+
+   public void setTariffa(double tariffa) {
+      this.tariffa = tariffa;
+   }
 }
