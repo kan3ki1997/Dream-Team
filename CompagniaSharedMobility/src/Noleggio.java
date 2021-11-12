3221002e22a2059eebd9114c67764ec00839f1e7
Alessandro <alessandro.ghio@protonmail.com>
2021-11-12 16:47:34 +0200

    Aggiornata documentazione

--- CompagniaSharedMobility/src/Noleggio.java
+++ CompagniaSharedMobility/src/Noleggio.java
@@ -2,78 +2,90 @@ import java.util.Date;
 
 public class Noleggio {
 
-    private Utente utente;
-    private Veicolo veicolo;
-    private Database database;
-    private Date dataInizio;
-    private Date dataFine;
+   private Utente utente;
+   private Veicolo veicolo;
+   private Database database;
+   private Date dataInizio;
+   private Date dataFine;
 
-    public Noleggio(Utente utente, Veicolo veicolo, Date dataInizio, Date dataFine) {
-        this.utente = utente;
-        this.veicolo = veicolo;
-        this.dataInizio = dataInizio;
-        this.dataFine = dataFine;
-    }
+   public Noleggio(Utente utente, Veicolo veicolo, Date dataInizio, Date dataFine) {
+      this.utente = utente;
+      this.veicolo = veicolo;
+      this.dataInizio = dataInizio;
+      this.dataFine = dataFine;
+   }
 
-    // metodi
-    public void registraUtente() {
-        database.registrazioneUtente(this.utente);
+   // metodi
+   public void registraUtente() {
+      database.registrazioneUtente(this.utente);
 
-    }
+   }
 
-    public void loginUtente() {
-        if (database.cercaUtente(utente) == false) System.out.println("utente non registrato");
-        else System.out.println("login effettuato con successo");
-    }
+   public void loginUtente() {
+      if (database.cercaUtente(utente) == false) System.out.println("utente non registrato");
+      else System.out.println("login effettuato con successo");
+   }
 
-    public boolean veicoliDisponibili() {
-        if (this.veicolo instanceof VeicoloBenzina && ((VeicoloBenzina) this.veicolo).getSerbatoio() < 10) return false;
-        else if (this.veicolo.affittato != false) return false;
-        else return true;
-    }
+   public boolean veicoliDisponibili() {
+      if (this.veicolo instanceof VeicoloBenzina && ((VeicoloBenzina) this.veicolo).getSerbatoio() < 10) return false;
+      else if (this.veicolo.affittato != false) return false;
+      else return true;
+   }
 
-    public void affittoVeicolo() {
-        this.veicolo.setAffittato(true);
-        setDataInizio(dataInizio);
+   public void affittoVeicolo() {
+      this.veicolo.setAffittato(true);
+      setDataInizio(dataInizio);
 
-    }
+   }
 
-    public void fineNoleggio() {
-        this.veicolo.setAffittato(false);
-        setDataFine(dataFine);
-    }
+   public void fineNoleggio() {
+      this.veicolo.setAffittato(false);
+      setDataFine(dataFine);
+   }
 
-    // getters and setters
+   // getters and setters
 
-    public Utente getUtente() {
-        return utente;
-    }
+   public Utente getUtente() {
+      return utente;
+   }
 
-    public void setUtente(Utente utente) {
-        this.utente = utente;
-    }
+   /*
+   @param utente L'utente che noleggia il veicolo
+    */
+   public void setUtente(Utente utente) {
+      this.utente = utente;
+   }
 
-    public Veicolo getVeicolo() {
-        return veicolo;
-    }
+   public Veicolo getVeicolo() {
+      return veicolo;
+   }
 
-    public void setVeicolo(Veicolo veicolo) {
-        this.veicolo = veicolo;
-    }
+   /*
+   @param veicolo Il veicolo da noleggiare
+    */
+   public void setVeicolo(Veicolo veicolo) {
+      this.veicolo = veicolo;
+   }
 
-    public Date getDataInizio() {
-        return dataInizio;
-    }
+   public Date getDataInizio() {
+      return dataInizio;
+   }
 
-    public void setDataInizio(Date dataInizio) {
-        this.dataInizio = dataInizio;
-    }
+   /*
+   @param dataInizio Data inizio noleggio
+    */
+   public void setDataInizio(Date dataInizio) {
+      this.dataInizio = dataInizio;
+   }
 
-    public Date getDataFine() {
-        return dataFine;
-    }
+   public Date getDataFine() {
+      return dataFine;
+   }
 
-    public void setDataFine(Date dataFine) {
-        this.dataFine = dataFine;
-    }
+   /*
+   @param dataFine Data fine noleggio
+    */
+   public void setDataFine(Date dataFine) {
+      this.dataFine = dataFine;
+   }
 }
