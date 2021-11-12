e935e458457694f8e6725db2f640fc66a7f4c88d
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2021-11-12 13:47:39 +0100

    Nuova classe automobile con attributi e costruttore

---
+++ CompagniaSharedMobility/src/Automobile.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Automobile extends VeicoloBenzina {
+    private List<Patente> patenteNecessaria = new ArrayList<Patente>();
+
+    public Automobile() {
+        patenteNecessaria.add(Patente.A);
+        patenteNecessaria.add(Patente.B);
+    }
+}
