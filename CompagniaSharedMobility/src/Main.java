0d09097ebad7cac91a953a724d890c3f8f9b4d29
Daniele Aru <22505437+ddeler@users.noreply.github.com>
2021-11-12 14:21:59 +0100

    Aggiunti VeicoloElettrico, MonopattinoElettrico. Iniziato il main

--- CompagniaSharedMobility/src/Main.java
+++ CompagniaSharedMobility/src/Main.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        int x = 0;
-        System.out.println("ciao" + x);
+        ArrayList<Automobile> automobili = new ArrayList<Automobile>();
+        ArrayList<Scooter> scooter = new ArrayList<Scooter>();
+        ArrayList<Furgoncino> furgoncini = new ArrayList<Furgoncino>();
+        ArrayList<MonopattinoElettrico> monopattini = new ArrayList<MonopattinoElettrico>();
+        ArrayList<Bici> bici = new ArrayList<Bici>();
+        ArrayList<Utente> utenti = new ArrayList<Utenti>();
     }
 }
---
+++ CompagniaSharedMobility/src/VeicoloElettrico.java
@@ -0,0 +1,11 @@
+public abstract class VeicoloElettrico extends VeicoloImmatricolato{
+    int batteria;
+
+    public int getBatteria(){
+        return batteria;
+    }
+
+    public void setBatteria(int batteria){
+        this.batteria = batteria;
+    }
+}
