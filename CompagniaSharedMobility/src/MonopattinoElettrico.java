e2ddfceda3b699f287fe44d020eba020a0a6e519
Daniele Aru <22505437+ddeler@users.noreply.github.com>
2021-11-12 14:24:37 +0100

    Commento MonopattinoElettrico

--- CompagniaSharedMobility/src/MonopattinoElettrico.java
+++ CompagniaSharedMobility/src/MonopattinoElettrico.java
@@ -1,6 +1,6 @@
 public class MonopattinoElettrico extends VeicoloElettrico{
 
-    public MonopattinoElettrico(int id, double[] posizioneGPS, boolean affittato, double tariffa, int batteria) {
+    public MonopattinoElettrico(int id, double[] posizioneGPS, /*boolean affittato, */double tariffa, int batteria) {
         this.id = id;
         this.posizioneGPS = posizioneGPS;
         this.affittato = false; //Alla creazione di un veicolo, questo sarà sempre non affittato
