51c9acd7875219dd966d35a8db50abaf0dfbfa9c
Daniele Aru <22505437+ddeler@users.noreply.github.com>
2021-11-12 15:18:51 +0100

    Sistemati casini vari in MonopattinoElettrico e VeicoloElettrico

--- CompagniaSharedMobility/src/Main.java
+++ CompagniaSharedMobility/src/Main.java
@@ -10,4 +10,11 @@ public class Main {
         ArrayList<Bici> bici = new ArrayList<Bici>();
         ArrayList<Utente> utenti = new ArrayList<Utenti>();
     }
+
+    public void addMonopattino(ArrayList monopattini){
+        int indice = monopattini.size();
+        int id = monopattini.size() + 1;
+        double[] posizioneGPS = {0,0};
+        //monopattini.add(new MonopattinoElettrico(monopattini.size() + 1, posizioneGPS, ));
+    }
 }
