93a9bbf7ac4f447319315762b09f74373c6d279e
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2021-11-15 11:31:34 +0100

    RIP costruttore

--- CompagniaSharedMobility/src/VeicoloBenzina.java
+++ CompagniaSharedMobility/src/VeicoloBenzina.java
@@ -3,11 +3,6 @@ import java.util.List;
 public abstract class VeicoloBenzina extends VeicoloImmatricolato {
    private float serbatoio;
 
-   // constructor
-   public VeicoloBenzina(int id, double[] posizioneGPS, boolean affittato, double tariffa, float serbatoio, String targa, List<Patente> patenteNecessaria) {
-      super(id, posizioneGPS, affittato, tariffa, targa, patenteNecessaria);
-      this.serbatoio = serbatoio;
-   }
 
    public float getSerbatoio() {
       return serbatoio;
