535d51d68c82ec4c20d140d24b18131f5351ac1a
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2021-11-15 14:15:50 +0100

    aggiunta registrazione scooter

--- CompagniaSharedMobility/src/Scooter.java
+++ CompagniaSharedMobility/src/Scooter.java
@@ -4,14 +4,15 @@ import java.util.List;
 public class Scooter extends VeicoloBenzina{
     private boolean haCasco;
     private final List<Patente> patenteNecessaria = new ArrayList<Patente>();
+    Database database;
 
     public Scooter(int id, double[] posizioneGPS, boolean affittato, double tariffa, float serbatoio, boolean haCasco, String targa, List<Patente> patenteNecessaria) {
         super(id,posizioneGPS,targa,serbatoio,tariffa,patenteNecessaria);
         patenteNecessaria.add(Patente.A);
         patenteNecessaria.add(Patente.B);
+        database.registrazioneScooter(this);
 
         this.haCasco = haCasco;
-
         this.tariffa=0.5;
     }
 
