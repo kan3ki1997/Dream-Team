88d4c21e939946691e27d6f33cf9cf2731dc3ff0
Daniele Aru <22505437+ddeler@users.noreply.github.com>
2021-11-15 14:41:55 +0100

    Aggiunti metodi per l'aggiunta dei veicoli in Main

--- CompagniaSharedMobility/src/Main.java
+++ CompagniaSharedMobility/src/Main.java
@@ -1,4 +1,6 @@
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,20 +12,69 @@ public class Main {
       ArrayList<Bici> bici = new ArrayList<Bici>();
       ArrayList<Utente> utenti = new ArrayList<Utente>();
       Database database = new Database();
-      double[] PosizioneIniziale = {0,0};
 
-      Automobile ferrari = new Automobile (2, PosizioneIniziale, false, 25.0, 30.0f, "AA000AA");
-      Furgoncino piaggioPorter = new Furgoncino(3, PosizioneIniziale, false, 2, 30.0f, "BB123CC");
+      /*Automobile ferrari = new Automobile (2, PosizioneIniziale, false, 25.0, 30.0f, "AA000AA");
+      Furgoncino piaggioPorter = new Furgoncino(3, PosizioneIniziale, false, 2, 30.0f, "BB123CC");*/
 
    }
 
-   /*
-   @param monopattini La lista dei monopattini
-    */
-   public void addMonopattino(ArrayList monopattini) {
-      int indice = monopattini.size();
-      int id = monopattini.size() + 1;
-      double[] posizioneGPS = {0, 0};
-       monopattini.add(new MonopattinoElettrico(id, posizioneGPS));
+   public void addAutomobile(ArrayList automobili){
+       Scanner sc = new Scanner(System.in);
+       int indice = automobili.size();
+       int id = automobili.size() + 1;
+       System.out.print("Targa: ");
+       String targa = sc.nextLine();
+       automobili.add(new Automobile(id, targa));
    }
+
+    public void addScooter(ArrayList scooter){
+        Scanner sc = new Scanner(System.in);
+        int indice = scooter.size();
+        int id = scooter.size() + 1;
+        System.out.print("Targa: ");
+        String targa = sc.nextLine();
+        boolean haCasco = false;
+
+        /**
+         * O metti la risposta giusta o non uscirai mai più da questo loop infernale.
+         */
+        String risposta = "";
+        while(!risposta.equals("s") && !risposta.equals("n"))
+        {
+            System.out.print("Hai un casco? (s/n): ");
+            String rispostaTemp = sc.nextLine();
+            risposta = rispostaTemp.toLowerCase();
+            if(!risposta.equals("s") && !risposta.equals("n")
+                System.out.println("Scelta non corretta. Scrivi <s> per Sì e <n> per No.");
+            //e che famo se ha il casco o meno?
+        }
+        if (risposta == "s")
+            haCasco = true;
+        else
+            haCasco = false;
+
+        scooter.add(new Automobile(id, targa, haCasco));
+    }
+
+    public void addFurgoncino(ArrayList furgoncino) {
+        Scanner sc = new Scanner(System.in);
+        int indice = furgoncino.size();
+        int id = furgoncino.size() + 1;
+        System.out.print("Targa: ");
+        String targa = sc.nextLine();
+        furgoncino.add(new Automobile(id, targa));
+    }
+
+    public void addMonopattino(ArrayList monopattini) {
+        int indice = monopattini.size();
+        int id = monopattini.size() + 1;
+        monopattini.add(new MonopattinoElettrico(id));
+    }
+
+    public void addBici(ArrayList bici) {
+        int indice = bici.size();
+        int id = bici.size() + 1;
+        bici.add(new MonopattinoElettrico(id));
+    }
+
 }
