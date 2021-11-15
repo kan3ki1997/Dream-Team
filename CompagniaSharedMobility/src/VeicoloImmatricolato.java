5f3ea25173fd4525cf76ca2ec71b2251e9b4bf10
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2021-11-15 11:29:23 +0100

    RIP costruttore

--- CompagniaSharedMobility/src/VeicoloImmatricolato.java
+++ CompagniaSharedMobility/src/VeicoloImmatricolato.java
@@ -5,11 +5,4 @@ public abstract class VeicoloImmatricolato extends Veicolo {
    private final String targa;
    private final List<Patente> patenteNecessaria = new ArrayList<Patente>();
 
-   // constructor
-   public VeicoloImmatricolato(int id, double[] posizioneGPS, String targa, double tariffa, boolean affittato, ArrayList <Patente> patenteNecessaria) {
-      super(id, posizioneGPS, tariffa, affittato);
-      this.targa = targa;
-      this.patenteNecessaria = patenteNecessaria;
-
-   }
 }
