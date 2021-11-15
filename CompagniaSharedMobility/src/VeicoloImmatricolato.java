9fd9180668819531d7a3d66cf9c701af627b9f1b
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2021-11-15 11:58:21 +0100

    resuscitato costruttore

--- CompagniaSharedMobility/src/VeicoloImmatricolato.java
+++ CompagniaSharedMobility/src/VeicoloImmatricolato.java
@@ -4,4 +4,9 @@ import java.util.List;
 public abstract class VeicoloImmatricolato extends Veicolo {
    private final String targa;
    private final List<Patente> patenteNecessaria = new ArrayList<Patente>();
+
+   public VeicoloImmatricolato(int id, double[] posizioneGPS, String targa) {
+      super(id, posizioneGPS);
+      this.targa = targa;
+   }
 }
