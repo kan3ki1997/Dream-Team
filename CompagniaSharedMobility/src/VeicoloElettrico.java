a9200bc0662c0bc599e6ac175dfcd893379b44d9
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2021-11-15 12:00:24 +0100

    resuscitato costruttore

--- CompagniaSharedMobility/src/VeicoloElettrico.java
+++ CompagniaSharedMobility/src/VeicoloElettrico.java
@@ -3,7 +3,9 @@ import java.util.List;
 public abstract class VeicoloElettrico extends VeicoloImmatricolato{
     private int batteria;
 
-    public VeicoloElettrico() {
+    public VeicoloElettrico(int id, double[] posizioneGPS, String targa, int batteria) {
+        super(id, posizioneGPS, targa);
+        this.batteria = batteria;
     }
 
     public int getBatteria(){
