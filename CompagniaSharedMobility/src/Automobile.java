ea4b17a1649157cf9ab4080411b89366cf5c9c80
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2021-11-15 15:04:48 +0100

    Sistemati errori segnalati da IntelliJ

--- CompagniaSharedMobility/src/Automobile.java
+++ CompagniaSharedMobility/src/Automobile.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class Automobile extends VeicoloBenzina {
-   private final Set<Patente> patenteNecessaria;
    Database database;
 
    // constructor
-   public Automobile(int id, double[] posizioneGPS, boolean affittato, double tariffa,
-                     float serbatoio, String targa) {
-      super(id, posizioneGPS, affittato, tariffa, serbatoio, targa, patenteNecessaria);
+   public Automobile(int id, double[] posizioneGPS, String targa, float serbatoio, double tariffa, List<Patente> patenteNecessaria) {
+      super(id, posizioneGPS, targa, serbatoio, tariffa, patenteNecessaria);
       patenteNecessaria.add(Patente.B);
    }
 
