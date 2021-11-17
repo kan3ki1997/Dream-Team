6d84b42cf2a7a7d8f61a529abb1f7425b28c0616
Daniele Aru <22505437+ddeler@users.noreply.github.com>
2021-11-17 09:55:09 +0100

    Continuazione della bozza del menu e SISTEMATA L'INDENTAZIONE

--- CompagniaSharedMobility/src/Main.java
+++ CompagniaSharedMobility/src/Main.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-      HashSet<Automobile> automobili = new HashSet<Automobile>();
-      HashSet<Scooter> scooter = new HashSet<Scooter>();
-      HashSet<Furgoncino> furgoncini = new HashSet<Furgoncino>();
-      HashSet<MonopattinoElettrico> monopattini = new HashSet<MonopattinoElettrico>();
-      HashSet<Bici> bici = new HashSet<Bici>();
-      HashSet<Utente> utenti = new HashSet<Utente>();
-      Database database = new Database();
+        HashSet<Automobile> automobili = new HashSet<Automobile>();
+        HashSet<Scooter> scooter = new HashSet<Scooter>();
+        HashSet<Furgoncino> furgoncini = new HashSet<Furgoncino>();
+        HashSet<MonopattinoElettrico> monopattini = new HashSet<MonopattinoElettrico>();
+        HashSet<Bici> bici = new HashSet<Bici>();
+        HashSet<Utente> utenti = new HashSet<Utente>();
+        Database database = new Database();
 
-      menu();
+        menu();
 
-      /*Automobile ferrari = new Automobile (2, PosizioneIniziale, false, 25.0, 30.0f, "AA000AA");
-      Furgoncino piaggioPorter = new Furgoncino(3, PosizioneIniziale, false, 2, 30.0f, "BB123CC");*/
-
+        /*Automobile ferrari = new Automobile (2, PosizioneIniziale, false, 25.0, 30.0f, "AA000AA");
+        Furgoncino piaggioPorter = new Furgoncino(3, PosizioneIniziale, false, 2, 30.0f, "BB123CC");*/
    }
 
    public void addAutomobile(HashSet automobili){
@@ -27,7 +26,7 @@ public class Main {
        automobili.add(new Automobile(id, targa));
    }
 
-    public void addScooter(HashSet scooter){
+   public void addScooter(HashSet scooter){
         Scanner sc = new Scanner(System.in);
         int id = scooter.size() + 1;
         System.out.print("Targa: ");
@@ -55,7 +54,7 @@ public class Main {
         scooter.add(new Scooter(id, targa, haCasco));
     }
 
-    public void addFurgoncino(HashSet furgoncino) {
+   public void addFurgoncino(HashSet furgoncino) {
         Scanner sc = new Scanner(System.in);
         int id = furgoncino.size() + 1;
         System.out.print("Targa: ");
@@ -63,48 +62,54 @@ public class Main {
         furgoncino.add(new Furgoncino(id, targa));
     }
 
-    public void addMonopattino(HashSet monopattini) {
+   public void addMonopattino(HashSet monopattini) {
         int id = monopattini.size() + 1;
         double[] posizioneGPS = new double[]{0,0};
         monopattini.add(new MonopattinoElettrico(id, posizioneGPS));
     }
 
-    public void addBici(HashSet bici) {
+   public void addBici(HashSet bici) {
         int id = bici.size() + 1;
         bici.add(new Bici(id));
     }
 
-    public static void menu() throws InterruptedException {
-        Scanner sc = new Scanner(System.in);
-        int scelta = 0;
+   public static void menu() throws InterruptedException {
+       Scanner sc = new Scanner(System.in);
+       int scelta = 0;
 
-        while (scelta != 1) {
-            System.out.println("\nCosa vuoi fare?");
-            System.out.println("1. Aggiungi un veicolo");
-            System.out.println("2. Esci");
-            System.out.print("Scelta: ");
-            scelta = sc.nextInt();
-            System.out.println("\n");
+       while (scelta != 1) {
+           System.out.println("\nCosa vuoi fare?");
+           System.out.println("1. Aggiungi un veicolo");
+           System.out.println("2. Aggiungi utente");
+           System.out.println("3. Esci");
+           System.out.print("Scelta: ");
+           scelta = sc.nextInt();
+           System.out.println("\n");
 
-            switch (scelta){
-                case 1:
-                    menuVeicoli();
-                    break;
+           switch (scelta){
+               case 1:
+                   menuVeicoli();
+                   break;
 
-                case 2:
-                    System.out.println("Addio.");
-                    break;
+               case 2:
+                  addUtente();
 
-                default:
-                    System.out.println("Scelta non corretta.");
-                    break;
-            }
-            if (scelta == 2)
-                break;
-            else
-                scelta = 0;
-            }
-        }
-    }
+               case dibbase:
+                   System.out.println("Addio.");
+                   break;
 
+               default:
+                   System.out.println("Scelta non corretta.");
+                   break;
+           }
+           if (scelta == dibbase)
+               break;
+           else
+               scelta = 0;
+       }
+   }
+
+   public void addUtente(){
+
+   }
 }
