6493ca43db7be2ef558d5c1e13fd50191f0c569e
SolangeBV <92978465+SolangeBV@users.noreply.github.com>
2021-11-17 11:44:03 +0100

    aggiunto menuNoleggi

--- CompagniaSharedMobility/src/Main.java
+++ CompagniaSharedMobility/src/Main.java
@@ -136,13 +136,14 @@ public class Main {
        System.out.println("\nCosa vuoi fare?");
        System.out.println("1. Aggiungi un veicolo");
        System.out.println("2. Aggiungi utente");
-       System.out.println("3. Esci");
+       System.out.println("3. Noleggia un veicolo");
+       System.out.println("4. Esci");
        System.out.print("Scelta: ");
        int scelta = sc.nextInt();
        return scelta;
    }
 
-   public static int MenuVeicoli() {
+   public static int MenuNoleggi() {
         Scanner sc = new Scanner(System.in);
        System.out.println("Quale veicolo vuoi noleggiare?");
        System.out.println("1. Auto");
@@ -153,4 +154,16 @@ public class Main {
        System.out.println("6. Torna al menu principale");
        System.out.println("7. Esci");
    }
+
+    public static int MenuVeicoli() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quale veicolo vuoi aggiungere?");
+        System.out.println("1. Auto");
+        System.out.println("2. Furgoncino");
+        System.out.println("3. Scooter");
+        System.out.println("4. Bici");
+        System.out.println("5. Monopattino");
+        System.out.println("6. Torna al menu principale");
+        System.out.println("7. Esci");
+    }
 }
