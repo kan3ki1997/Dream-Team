ee7a565958092efbbd7a4fd45573e2218d6ed526
Niki <80227107+kan3ki1997@users.noreply.github.com>
2021-11-17 15:31:56 +0100

    s

--- CompagniaSharedMobility/src/Noleggio.java
+++ CompagniaSharedMobility/src/Noleggio.java
@@ -2,106 +2,106 @@ import java.util.Date;
 
 public class Noleggio {
 
-   private Utente utente;
-   private Veicolo veicolo;
-   private Database database;
-   private Date dataInizio;
-   private Date dataFine;
-   private int id;
+    private Utente utente;
+    private Veicolo veicolo;
+    private Database database;
+    private Date dataInizio;
+    private Date dataFine;
+    private int id;
 
-   public Noleggio(Utente utente, Veicolo veicolo, Date dataInizio, Date dataFine, int id) {
-      this.utente = utente;
-      this.veicolo = veicolo;
-      this.dataInizio = dataInizio;
-      this.dataFine = dataFine;
-      this.id = id;
-   }
+    public Noleggio(Utente utente, Veicolo veicolo, Date dataInizio, Date dataFine, int id) {
+        this.utente = utente;
+        this.veicolo = veicolo;
+        this.dataInizio = dataInizio;
+        this.dataFine = dataFine;
+        this.id = id;
+    }
 
-   // metodi
-   public void registraUtente(Utente utente) {
-      database.registrazioneUtente(utente);
-   }
+    // metodi
+    public void registraUtente(Utente utente) {
+        database.registrazioneUtente(utente);
+    }
 
-   public void registraNoleggio(Noleggio noleggio) {
-      database.registrazioneNoleggio(noleggio);
-   }
+    public void registraNoleggio(Noleggio noleggio) {
+        database.registrazioneNoleggio(noleggio);
+    }
 
-   public int getId() {
-      return id;
-   }
+    public void loginUtente() {
+        if (!database.cercaUtente(id)) System.out.println("utente non registrato");
+        else System.out.println("login effettuato con successo");
+    }
 
-   public void setId(int id) {
-      this.id = id;
-   }
+    public boolean veicoliDisponibili() {
+        if (this.veicolo instanceof VeicoloBenzina && ((VeicoloBenzina) this.veicolo).getSerbatoio() < 10)
+            return false; // serbatoio non sufficente
+        else if (this.veicolo.isAffittato() == true) return false; // il veicolo è affittato
+        else if (this.veicolo instanceof Scooter || this.veicolo instanceof Bici && !this.utente.hasCasco()) {
+            return false; // non casco munito
+        } else return true;
+    }
 
-   public void loginUtente() {
-      if (!database.cercaUtente(id)) System.out.println("utente non registrato");
-      else System.out.println("login effettuato con successo");
-   }
+    public void affittoVeicolo() {
+        this.veicolo.setAffittato(true);
+        setDataInizio(dataInizio);
 
-   public boolean veicoliDisponibili() {
-      if (this.veicolo instanceof VeicoloBenzina && ((VeicoloBenzina) this.veicolo).getSerbatoio() < 10) return false; // serbatoio non sufficente
-      else if (this.veicolo.isAffittato() == true) return false; // il veicolo è affittato
-      else if (this.veicolo instanceof Scooter || this.veicolo instanceof Bici && !this.utente.hasCasco()){
-         return false; // non casco munito
-      }
-      else return true;
-   }
+    }
 
-   public void affittoVeicolo() {
-      this.veicolo.setAffittato(true);
-      setDataInizio(dataInizio);
+    public void fineNoleggio() {
+        this.veicolo.setAffittato(false);
+        setDataFine(dataFine);
+    }
 
-   }
+    // getters and setters
 
-   public void fineNoleggio() {
-      this.veicolo.setAffittato(false);
-      setDataFine(dataFine);
-   }
+    public int getId() {
+        return id;
+    }
 
-   // getters and setters
+    public void setId(int id) {
+        this.id = id;
+    }
 
-   public Utente getUtente() {
-      return utente;
-   }
+    public Utente getUtente() {
+        return utente;
+    }
 
-   /*
-   @param utente L'utente che noleggia il veicolo
-    */
-   public void setUtente(Utente utente) {
-      this.utente = utente;
-   }
+    /*
+    @param utente L'utente che noleggia il veicolo
+     */
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
 
-   public Veicolo getVeicolo() {
-      return veicolo;
-   }
+    public Veicolo getVeicolo() {
+        return veicolo;
+    }
 
-   /*
-   @param veicolo Il veicolo da noleggiare
-    */
-   public void setVeicolo(Veicolo veicolo) {
-      this.veicolo = veicolo;
-   }
+    /*
+    @param veicolo Il veicolo da noleggiare
+     */
+    public void setVeicolo(Veicolo veicolo) {
+        this.veicolo = veicolo;
+    }
 
-   public Date getDataInizio() {
-      return dataInizio;
-   }
+    public Date getDataInizio() {
+        return dataInizio;
+    }
 
-   /*
-   @param dataInizio Data inizio noleggio
-    */
-   public void setDataInizio(Date dataInizio) {
-      this.dataInizio = dataInizio;
-   }
+    /*
+    @param dataInizio Data inizio noleggio
+     */
+    public void setDataInizio(Date dataInizio) {
+        this.dataInizio = dataInizio;
+    }
 
-   public Date getDataFine() {
-      return dataFine;
-   }
+    public Date getDataFine() {
+        return dataFine;
+    }
 
-   /*
-   @param dataFine Data fine noleggio
-    */
-   public void setDataFine(Date dataFine) {
-      this.dataFine = dataFine;
-   }
+    /*
+    @param dataFine Data fine noleggio
+     */
+    public void setDataFine(Date dataFine) {
+        this.dataFine = dataFine;
+    }
 }
